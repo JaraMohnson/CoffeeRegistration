1ac4a116a31d666e453b5ef45adbfabd10c0a9a0
JaraMohnson <jaramohnson@gmail.com>
2022-07-21 17:01:10 -0400

    1st

---
+++ RegistrationLab/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegistrationLab.Models;
+using System.Diagnostics;
+
+namespace RegistrationLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult UserInput()
+        {
+            return View();
+        }
+
+        public IActionResult UserResult(User u)
+        {
+            return View(u);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
